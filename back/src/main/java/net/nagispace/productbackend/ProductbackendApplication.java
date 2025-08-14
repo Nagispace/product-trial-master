ca25e4cdc4e91f006e6958a2fcccdf9e2df0658c
Nicolas Jean-Baptiste Coulon <nicolas@nagispace.net>
2025-08-14 20:38:24 +0200

    Springboot basse + openAPI file

---
+++ back/src/main/java/net/nagispace/productbackend/ProductbackendApplication.java
@@ -0,0 +1,13 @@
+package net.nagispace.productbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductbackendApplication.class, args);
+    }
+
+}
---
+++ back/src/test/java/net/nagispace/productbackend/ProductBackendApplicationTests.java
@@ -0,0 +1,12 @@
+package net.nagispace.productbackend;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class ProductBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+        assertTrue(true);
+    }
+}
